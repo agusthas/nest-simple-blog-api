5978adb8db8d253c257d527888631a053d9f5088
agusthas <suzumantan@gmail.com>
2021-07-12 00:14:03 +0700

    feat: :sparkles: add validation / authorization for user input

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api/v1'); // setting a global prefix -> routes become 'api/v1/..../'
+  // setting a global prefix -> routes become 'api/v1/..../'
+  app.setGlobalPrefix('/api/v1');
+  // handle all user input validation
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,19 +1,21 @@
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 
+import { LocalAuthGuard } from './localAuth.guard';
 import { AuthService } from './auth.service';
 import { UserDto } from '../users/user.dto';
+import { DoesUserExistGuard } from '../../core/guards/doesUserExist.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
     return await this.authService.login(req.user);
   }
 
+  @UseGuards(DoesUserExistGuard)
   @Post('signup')
   async signUp(@Body() user: UserDto) {
     return await this.authService.create(user);

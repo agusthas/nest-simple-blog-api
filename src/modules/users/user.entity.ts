aafdd7a7306f066a96b9158a2091bc0465fed92e
agusthas <suzumantan@gmail.com>
2021-07-11 22:41:27 +0700

    feat: :sparkles: add users module

---
+++ src/modules/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table
+export class User extends Model {
+  @Column({ type: DataType.STRING, allowNull: false })
+  name: string;
+
+  @Column({ type: DataType.STRING, allowNull: false, unique: true })
+  email: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  password: string;
+
+  @Column({ type: DataType.ENUM, values: ['male', 'female'], allowNull: false })
+  gender: string;
+}
